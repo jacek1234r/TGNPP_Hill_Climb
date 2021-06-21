46b10cb01cd4fe3bd57ffdc26ed636f4b4555bf7
Liderty <mateusz.liber@smcebi.edu.pl>
2021-06-21 13:33:53 +0200

    Added timer on lose

--- Assets/Scripts/HitDetector.cs
+++ Assets/Scripts/HitDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class HitDetector : MonoBehaviour {
@@ -8,13 +9,36 @@ public class HitDetector : MonoBehaviour {
     public GameObject MenuMenu1;
     public GameObject EndMenu;
     public static HitDetector obj;
-    public void Awake()
-    {
-        if (obj == null)
-        {
+    public GameObject countDownTimerText;
+
+    int countDownStartValue = 4;
+
+    public void Awake() {
+        if (obj == null) {
             obj = this;
+            
         }
     }
+
+    void countDownTimer() {
+        if (countDownStartValue > 0) {
+            if(countDownStartValue == 4) {
+                Debug.Log("GAME OVER");
+                countDownTimerText.GetComponent<TMPro.TextMeshProUGUI>().text = "GAME OVER";
+                countDownStartValue--;
+            } else {
+                Debug.Log("Timer: "+countDownStartValue);
+                countDownTimerText.GetComponent<TMPro.TextMeshProUGUI>().text = ""+countDownStartValue;;
+                countDownStartValue--;
+            }
+            
+            Invoke("countDownTimer", 1.0f);
+        } else {
+            Debug.Log("Restarting...");
+            resetLevel();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Ground")) {
             Debug.Log(" Hit Any!");
@@ -26,19 +50,16 @@ public class HitDetector : MonoBehaviour {
             //AdsManager Ad = new AdsManager();
             //Ad.ShowInterstitialAd();
             SoundManager.PlaySound(SoundManager.Sound.Crash);
-            StartCoroutine( waiter() );
-
-
-        }
-        IEnumerator waiter( )
-         {
-            yield return new WaitForSeconds(4);
-            HitDetector.obj.MenuMenu.SetActive(false);
-            HitDetector.obj.MenuMenu1.SetActive(true);
-            HitDetector.obj.EndMenu.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //StartCoroutine( waiter() );
+            countDownTimer();
 
         }
+    }
 
+    private void resetLevel() {
+        HitDetector.obj.MenuMenu.SetActive(false);
+        HitDetector.obj.MenuMenu1.SetActive(true);
+        HitDetector.obj.EndMenu.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
